60f7bf37d0a27e236b74a4b65fdc25fc7d79a180
zekepari <pariezekiel@gmail.com>
2024-09-08 15:53:27 +1000

    update backend

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # GovHack 2024 - HikeAware
 
+
 ## The Product
-Our project had a two-pronged approach: to both aid in citizen science and local animal awareness from an app, and to help provide greater data awareness and analysis capabilities to the local council.To do so, we have created two outputs: the app [HikeAware](https://hikeaware.vercel.app/), and an associated backend data dashboard, accessible directly through the app, or via its own address. 
+Our project had a two-pronged approach: to both aid in citizen science and local animal awareness from an app, and to help provide greater data awareness and analysis capabilities to the local council.To do so, we have created two outputs: the app [HikeAware](https://hikeaware.vercel.app/), and an associated backend data dashboard, accessible directly through the app, or via its own address. Our video explaining our product is available here:
 
 
 
 ## Technical Specifications
+### HikeAware App
+The HikeAware app is built using NextJS. It utilised a responsive mobile-first design to deliver a seamless user experience. Styling ws accomplished using Tailwind CSS. An interactive map is implemented using the Google Maps API.
 
 ### Dashboard
-The HikeAware dashboard was created using the Python Plotly Dash library. It is hosted on an ARDC Nectar cloud virtual machine. It can be accessed [here](http://203.101.226.242:8050/).
+The HikeAware dashboard was created using the Python Plotly Dash library. It is hosted on an ARDC Nectar cloud virtual machine. It can be accessed [here](http://203.101.226.242:8050/). It utilises several data sets that have been combined into two CSV files that are loaded into the app via Pandas. Data analysis is accomplished using Numpy. 
 
 ## The Challenge
 We have entered into the challenge [Smart infrastructure for data-driven decision making](https://hackerspace.govhack.org/challenges/smart_infrastructure_for_data_driven_decision_making).
@@ -18,6 +21,7 @@ We are utilising the following datasets:
 + Mary Cairncross Scenic Reserve NCount WiFi Counter
 + Mary Cairncross Scenic Reserve Atmos Weather Station
 + Mary Cairncross Scenic Reserve Milesight Occupancy Counter
++ Mary Cairncross Scenic Reserve SMT100a Soil Moisture Sensor
 + Sugar Bag Road Recreation Reserve Atmos Weather Station
 + Sugar Bag Road Recreation Reserve Farmo PIR Counter
 
--- dashboard/app.py
+++ dashboard/app.py
@@ -7,6 +7,7 @@ import numpy as np
 
 
 # Load and clean data
+# Mary Cairncross data
 df = pd.read_csv('ALLDATA.csv')
 df.replace('na', pd.NA, inplace=True)
 df['datetime'] = pd.to_datetime(df['datetime'], format='%d/%m/%Y %H:%M')
@@ -19,6 +20,22 @@ df['red_necked_wallaby'] = pd.to_numeric(df['red_necked_wallaby'], errors='coerc
 df['great_barred_frog'] = pd.to_numeric(df['great_barred_frog'], errors='coerce')
 df['yellow_throated_scrubwren'] = pd.to_numeric(df['yellow_throated_scrubwren'], errors='coerce')
 df['noisy_pitta'] = pd.to_numeric(df['noisy_pitta'], errors='coerce')
+df['soilMoisture'] = pd.to_numeric(df['soilMoisture'], errors='coerce')
+# Sugar bag road data
+# For production turn this into a function to save space. Not enough time now.
+df_sbr = pd.read_csv('ALLDATA_sbr.csv')
+df_sbr.replace('na', pd.NA, inplace=True)
+df_sbr['datetime'] = pd.to_datetime(df['datetime'], format='%d/%m/%Y %H:%M')
+df_sbr['peopleIn'] = pd.to_numeric(df['peopleIn'], errors='coerce')
+df_sbr['peopleOut'] = pd.to_numeric(df['peopleOut'], errors='coerce')
+df_sbr['nonNegDigActivity'] = pd.to_numeric(df['nonNegDigActivity'], errors='coerce')
+df_sbr['relativeHumidity'] = pd.to_numeric(df['relativeHumidity'], errors='coerce')
+df_sbr['pademelon'] = pd.to_numeric(df['pademelon'], errors='coerce')
+df_sbr['red_necked_wallaby'] = pd.to_numeric(df['red_necked_wallaby'], errors='coerce')
+df_sbr['great_barred_frog'] = pd.to_numeric(df['great_barred_frog'], errors='coerce')
+df_sbr['yellow_throated_scrubwren'] = pd.to_numeric(df['yellow_throated_scrubwren'], errors='coerce')
+df_sbr['noisy_pitta'] = pd.to_numeric(df['noisy_pitta'], errors='coerce')
+
 
 # Group by date and calculate daily aggregates with max temperature
 daily_df = df.set_index('datetime').resample('D').agg({
@@ -32,8 +49,24 @@ daily_df = df.set_index('datetime').resample('D').agg({
     'red_necked_wallaby': 'sum',
     'great_barred_frog': 'sum',
     'yellow_throated_scrubwren': 'sum',
-    'noisy_pitta': 'sum'
+    'noisy_pitta': 'sum',
+    'soilMoisture': 'mean'
 }).reset_index()
+
+daily_df_sbr = df_sbr.set_index('datetime').resample('D').agg({
+    'nonNegDigActivity': 'sum',
+    'relativeHumidity': 'mean',
+    'airTemperature': 'max',
+    'precipitation': 'sum',
+    'windSpeed': 'mean',
+    'pademelon': 'sum',
+    'red_necked_wallaby': 'sum',
+    'great_barred_frog': 'sum',
+    'yellow_throated_scrubwren': 'sum',
+    'noisy_pitta': 'sum',
+}).reset_index()
+
+
 
 people_df = df.set_index('datetime').resample('D').agg({
     'relativeHumidity': 'mean',
@@ -45,7 +78,8 @@ people_df = df.set_index('datetime').resample('D').agg({
     'red_necked_wallaby': 'sum',
     'great_barred_frog': 'sum',
     'yellow_throated_scrubwren': 'sum',
-    'noisy_pitta': 'sum'
+    'noisy_pitta': 'sum',
+    'soilMoisture': 'mean'
 })
 people_df = people_df.dropna(subset=['peopleIn'])
 
@@ -59,76 +93,138 @@ dig_df = df.set_index('datetime').resample('D').agg({
     'red_necked_wallaby': 'sum',
     'great_barred_frog': 'sum',
     'yellow_throated_scrubwren': 'sum',
-    'noisy_pitta': 'sum'
+    'noisy_pitta': 'sum',
+    'soilMoisture': 'mean'
 })
 dig_df = dig_df.dropna(subset=['nonNegDigActivity'])
 
-# Calculate correlation matrix
+dig_df_sbr = df.set_index('datetime').resample('D').agg({
+    'relativeHumidity': 'mean',
+    'airTemperature': 'max',
+    'precipitation': 'sum',
+    'nonNegDigActivity': 'sum',
+    'windSpeed': 'mean',
+    'pademelon': 'sum',
+    'red_necked_wallaby': 'sum',
+    'great_barred_frog': 'sum',
+    'yellow_throated_scrubwren': 'sum',
+    'noisy_pitta': 'sum',
+
+})
+dig_df_sbr = dig_df.dropna(subset=['nonNegDigActivity'])
+
+# Calculate correlation matrices
 correlation_matrix = daily_df[['nonNegDigActivity', 'peopleIn', 'airTemperature', 
                                'precipitation', 'relativeHumidity', 'windSpeed',
                                'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren',
-                               'noisy_pitta']].corr()
+                               'noisy_pitta', 'soilMoisture']].corr()
 corr_dig_df = dig_df[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed',
-                      'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta']].corr()
+                      'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta', 'soilMoisture']].corr()
 corr_people_df = people_df[['peopleIn', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed',
-                            'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta']].corr()
+                            'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta', 'soilMoisture']].corr()
+
+# SBR
+correlation_matrix_sbr = daily_df[['nonNegDigActivity', 'airTemperature', 
+                               'precipitation', 'relativeHumidity', 'windSpeed',
+                               'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren',
+                               'noisy_pitta']].corr()
+corr_dig_df_sbr = dig_df_sbr[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed',
+                      'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta']].corr()
+
 
 # Create a Dash application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# CSS
+container_style = {
+    'padding': '30px',
+    'backgroundColor': 'rgb(187 247 208)'#'#B1FFCA'
+}
+
+header_style = {
+    'color': '#004d40',
+    'textAlign': 'center'
+}
+row_style = {
+    'paddingTop': '20px', 'paddingBottom': '20px'
+}
+
 # Example animals: 'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta'
 # App layout
-app.layout = dbc.Container([
-    html.H1("Wildlife Park Data Visualization"),
-    html.H1('General Statistics'),
+app.layout = dbc.Container(style=container_style, children=[
+    html.Div([html.H1("HikeAware - Dashboard", className='header'),
+    html.H2('Mary Cairncross Park'),
+    html.H3('General Statistics'),
     dbc.Row([
         dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='airTemperature', title='Max Air Temperature Per Day')), md=6),
-        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='peopleIn', title='Total Visitor Count Per Day')), md=6)
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='soilMoisture', title='Average Soil Moisture')), md=6)
     ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='nonNegDigActivity', title='Total Digital Activity Per Day')), md=12)
+    dbc.Row(style=row_style, children=[
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='peopleIn', title='Total Visitor Count Per Day')), md=6),
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='nonNegDigActivity', title='Total Digital Activity Per Day')), md=6)
     ]),
-    html.H2('Digital Activity'),
-    dbc.Row([
+    html.H3('Human Activity'),
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='airTemperature', title='Daily Digital Activity vs Max Air Temperature')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='windSpeed', title='Daily Digital Activity vs Mean Wind Speed')), md=6)
     ]),
-    dbc.Row([
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='relativeHumidity', title='Daily Digital Activity vs Relative Humidity')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='precipitation', title='Daily Digital Activity vs Precipitation')), md=6)
     ]),
-    html.H2('People In'),
-    dbc.Row([
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='airTemperature', title='Daily People In vs Max Air Temperature')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='windSpeed', title='Daily People In vs Mean Wind Speed')), md=6)
     ]),
-    dbc.Row([
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='relativeHumidity', title='Daily People In vs Relative Humidity')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='precipitation', title='Daily People In vs Precipitation')), md=6)
     ]),
     html.H2('Animal Sightings'),
-    dbc.Row([
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='pademelon',
-                                            title='Daily Digital Activity vs Total Pademelon Sightings',
+                                            title='Daily Digital Activity vs Total Pademelon Sightings (MOCK DATA)',
                                             labels = {'nonNegDigitalActivity': 'Digital Activity','pademelon': 'Pademelon Sightings'})), md=12)
     ]),
-    dbc.Row([
+    dbc.Row(style=row_style, children=[
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='red_necked_wallaby',
-                           title='Daily Digital Activity vs Total Red Necked Wallaby Sightings',
+                           title='Daily Digital Activity vs Total Red Necked Wallaby Sightings (MOCK DATA)',
                            labels = {'nonNegDigActivity': 'Digital Activity',
                                      'red_necked_wallaby': 'Red Necked Wallaby Sightings'})), md=12)
-    ])
-], fluid=True)  # Ensure the container is fluid
+    ])]),
+    html.H2('Sugar Bag Road Bike Trail'),
+    html.H3('General Statistics'),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=px.line(daily_df_sbr, x='datetime', y='airTemperature', title='Max Air Temperature Per Day')), md=6),
+        dbc.Col(dcc.Graph(figure=px.line(daily_df_sbr, x='datetime', y='nonNegDigActivity', 
+                                         title='Total Visitor Count Per Day',
+                                         labels={'nonNegDigActivity': 'Digital Activity Per Day'})), md=6)
+    ]),
+    html.H2('Human Activity'),
+    dbc.Row(style=row_style, children=[
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='airTemperature', title='Daily Digital Activity vs Max Air Temperature')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df_sbr, x='nonNegDigActivity', y='windSpeed', title='Daily Digital Activity vs Mean Wind Speed')), md=6)
+    ]),
+    dbc.Row(style=row_style, children=[
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df_sbr, x='nonNegDigActivity', y='relativeHumidity', title='Daily Digital Activity vs Relative Humidity')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df_sbr, x='nonNegDigActivity', y='precipitation', title='Daily Digital Activity vs Precipitation')), md=6)
+    ]),
+    html.H3('Animal Sightings'),
+    dbc.Row(style=row_style, children=[
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df_sbr, x='nonNegDigActivity', y='pademelon',
+                                            title='Daily Digital Activity vs Total Pademelon Sightings (MOCK DATA)',
+                                            labels = {'nonNegDigitalActivity': 'Digital Activity','pademelon': 'Pademelon Sightings'})), md=12)
+    ]),
+    dbc.Row(style=row_style, children=[
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df_sbr, x='nonNegDigActivity', y='red_necked_wallaby',
+                           title='Daily Digital Activity vs Total Red Necked Wallaby Sightings (MOCK DATA)',
+                           labels = {'nonNegDigActivity': 'Digital Activity',
+                                     'red_necked_wallaby': 'Red Necked Wallaby Sightings'})), md=12)
+    ]),
 
-# Deploy local port to internet. WARNING: Temporary only. This is a major security risk.
-# For proper development, set up actual server. This is only for competition judging.
-"""from pyngrok import ngrok
-def start_ngrok():
-    ngrok_tunnel = ngrok.connect(8050)
-    print(' * Tunnel URL:', ngrok_tunnel.public_url)"""
+    ], fluid=True)  # Ensure the container is fluid
+
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050)
-    #start_ngrok()
-    #app.run_server(debug=True, port=8050, use_reloader=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
