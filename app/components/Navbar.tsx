a81962bea7e6f09ea803600f57431597e7ff0750
zekepari <pariezekiel@gmail.com>
2024-09-08 10:40:08 +1000

    dashboard link

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <nav className="p-4 flex justify-between items-center gap-4">
-            <Link href="/signin"><CircleUserRound className="flex-shrink-0 hover:stroke-blue-500"/></Link>
+            <Link href="http://203.101.226.242:8050"><CircleUserRound className="flex-shrink-0 hover:stroke-blue-500"/></Link>
             <div className="rounded-full w-full bg-neutral-100 h-10 border-black border flex items-center px-4">
                 <Search className="text-gray-400 mr-2" />
                 <input
